a0ba0163f1dc20a85bb55755234ed66167575f7f
Abhinash1047 <keshawarabhinash@gmail.com>
2025-04-29 22:17:31 +0530

    code add by work1

--- GitProj01/src/main/java/com/nt/Git/App.java
+++ GitProj01/src/main/java/com/nt/Git/App.java
@@ -4,6 +4,8 @@ package com.nt.Git;
  * Hello world!
  */
 public class App {
+	
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hell abhi");
---
+++ GitProj01/src/main/java/com/nt/Git/Demo.java
@@ -0,0 +1,10 @@
+package com.nt.Git;
+
+public class Demo 
+{
+	public double sumOfTwo(double x,double y)
+	{
+		return x+y;
+		
+	}
+}
