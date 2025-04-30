020414880339eb8187a26b86a5e1d50cccac6fba
Abhinash1047 <keshawarabhinash@gmail.com>
2025-04-30 22:10:57 +0530

    work2 develop multiplication code

---
+++ GitProj01/src/main/java/com/nit/multi/Multiplication.java
@@ -0,0 +1,10 @@
+package com.nit.multi;
+
+public class Multiplication 
+{
+     public int multiplicationDemo(int x,int y)
+     {
+    	 System.out.println("Multiplication.multiplicationDemo()");
+    	 return x*y;
+     }
+}
